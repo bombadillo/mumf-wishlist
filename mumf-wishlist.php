518ff0e80c5992c3bc493055b84f3744cc4597e3
Chris Mumford <mumfordnsons@gmail.com>
2014-09-02 22:01:30 +0100

    Updated mumf-wishlist.php
    Now adding a button after product summary for users to add product to
    wishlist. We listen for product to finish loading then we load the
    assets for the plugin (currently only CSS).

--- mumf-wishlist.php
+++ mumf-wishlist.php
@@ -9,6 +9,10 @@
 	License: GPL2
 	*/
 
+	/**
+	 * Functions
+	 */	
+
 	// Called when plugin is activated
 	function mumf_wishlist_install() {
 		global $wpdb;
@@ -51,10 +55,46 @@
 
 	}
 
+	// Adds the wishlist button to the product page
+	function mumf_wishlist_add_button() {
+		echo '<div class="mumf-wishlist-button-container">
+		          <a href="javascript:void(0)" class="single_add_to_wishlist button alt">Add to wishlist</a>
+		      </div>';
+	}
+
+	// Gets the assets for the plugin
+	function mumf_wishlist_get_assets() {
+
+		// Get the styles.
+		wp_register_style('mumf-wishlist-styles', plugins_url('/assets/css/main.css', __FILE__ ));
+		wp_enqueue_style('mumf-wishlist-styles');	
+
+	}
+
+	/*****************************************************************************/
+
+	/**
+	 * Hooks
+	 */
+
 	// Register plugin activation hook to install function
 	register_activation_hook( __FILE__, 'mumf_wishlist_install' );
 
 	// Register plugin deactivation hook to uninstall function
 	register_deactivation_hook(__FILE__, 'mumf_wishlist_uninstall');
+
+	/*****************************************************************************/
+
+	/**
+	 * Actions
+	 */
+
+	// Calls the function after the product summary has loaded
+	add_action('woocommerce_single_product_summary', 'mumf_wishlist_add_button',40); 
+
+	// Calls the function after the product has fully loaded
+	add_action('woocommerce_after_single_product', 'mumf_wishlist_get_assets');
+
+	/*****************************************************************************/
 
 ?>

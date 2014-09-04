1679e80a630c0fd89b5323dc8bf928f7a56f5b57
Chris Mumford <mumfordnsons@gmail.com>
2014-09-04 12:41:31 +0100

    Added wishlist.class.php
    Added class for CRUD events for the wishlist

---
+++ assets/classes/wishlist.class.php
@@ -0,0 +1,103 @@
+<?php
+
+require_once("{$_SERVER['DOCUMENT_ROOT']}/wp-load.php");
+
+class Wishlist {
+
+	// Set private variables
+	private $user_id;
+
+	function __construct() {
+		// Set the current user for the class
+		global $current_user;
+		get_currentuserinfo();
+
+		$this->user_id = $current_user->ID;		
+	}
+	
+	/**
+	 * Name:     getFromWishlist
+	 * Purpose:  To get a single item from the wishlist
+	 * Params:   {int} iWishlistId   The post id of the item to retrieve
+	 * Returns:  +ve true
+	 *		     -ve false
+	 */
+	public function getFromWishlist($iWishlistId) {
+		global $wpdb;
+
+		// Prepare the query with params
+		$sql = $wpdb->prepare('SELECT id FROM wp_mumf_wishlist where post_id = %s AND user_id = %d', $iWishlistId, $this->user_id);
+
+		// Submit query
+		$wpdb->query($sql);
+
+		// Return the number of rows
+		return $wpdb->result->num_rows > 0;
+	}
+
+	/**
+	 * Name:     getAll
+	 * Purpose:  To get the entire wishlist
+	 * Returns:  +ve true
+	 *		     -ve false
+	 */
+	public function getAll() {
+		global $wpdb;
+
+		// Create date
+		$date = new DateTime();
+		
+		// Prepare the query with params
+		$wpdb->query("SELECT * FROM wp_mumf_wishlist WHERE user_id = $this->user_id");
+
+		// Set the classes fullWishlist to the new result
+		$this->fullWishlist = $wpdb->last_result;
+
+		// Return the number of rows
+		return $wpdb->result->num_rows > 0;
+	}	
+
+	/**
+	 * Name:     addToWishlist
+	 * Purpose:  To add a single item to the wishlist
+	 * Params:   {int} iWishlistId   The post id of the item to add
+	 * Returns:  +ve true
+	 *		     -ve false
+	 */
+	public function addToWishlist($iWishlistId) {
+		global $wpdb;
+
+		// Create date
+		$date = new DateTime();
+		
+		// Prepare the query with params
+		$sql = $wpdb->prepare("INSERT INTO wp_mumf_wishlist (post_id, user_id, date) VALUES (%s, %d, %s)", $iWishlistId, $this->user_id, $date->format('Y-m-d H:i:s'));
+
+		// Submit query
+		$wpdb->query($sql);
+
+		// Return the number of rows
+		return $wpdb->result > 0;
+	}
+
+	/**
+	 * Name:     deleteFromWishlist
+	 * Purpose:  To delete a single item from the wishlist
+	 * Params:   {int} iWishlistId   The post id of the item to delete
+	 * Returns:  +ve true
+	 *		     -ve false
+	 */
+	public function deleteFromWishlist($iWishlistId) {
+		global $wpdb;
+
+		// Prepare the query with params
+		$sql = $wpdb->prepare("DELETE FROM wp_mumf_wishlist WHERE post_id = %s && user_id = %d", $iWishlistId, $this->user_id);
+
+		// Submit query
+		$wpdb->query($sql);
+
+		// Return the number of rows
+		return $wpdb->result > 0;
+	}
+
+}

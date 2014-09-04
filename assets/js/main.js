76264ce375b5cd818576ce2aced48ce5013bea08
Chris Mumford <mumfordnsons@gmail.com>
2014-09-04 12:43:17 +0100

    Updated main.js
    Wait for DOM to load and add wishlist button to main menu. Listen for
    view all button to be clicked and fetch from server. Added display of
    errors based on server response.

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@
 	 * Global Variables
 	 **/
 
-	 var oUrls = { add: '/wp-content/plugins/mumf-wishlist/action.php' };
+	 var oUrls = { actions: '/wp-content/plugins/mumf-wishlist/action.php' };
 
 	 /*****************************************************************************/
 
@@ -12,23 +12,43 @@
 	 * Event listeners
 	 **/
 
-	 // Listen for the add to wishlist button being clicked
-	 $(document).on('click', '.single_add_to_wishlist', function() {
+	// Listen for DOM load
+	$(document).on('ready', function() {
+		// Call function to add the view wishlist button
+		addViewWishlistToMenu();
+	});
 
-	 	// Get the value of the product id
-	 	var productId = $('form.cart').find('input[name=add-to-cart]').val();
-	 	// Object to hold our parameters for the request.
-	 	var data = { productId: productId, action: 'add' };
+	// Listen for the add to wishlist button being clicked
+	$(document).on('click', '.single_add_to_wishlist', function() {
+
+		// Get the value of the product id
+		var productId = $('form.cart').find('input[name=add-to-cart]').val();
+		// Object to hold our parameters for the request.
+		var data = { id: productId, action: 'add' };
 
 		$.ajax({
 			type: "POST",
-			url: oUrls.add,
+			url: oUrls.actions,
 			data: data,
 			success: onWishListAdd,
 			dataType: 'text'
 		});	 	
 
-	 });
+	});
+
+	// Listen for the view wishlist button being clicked
+	$(document).on('click', '.mumf-wishlist-view', function() {
+		// Object to hold our parameters for the request.
+		var data = { action: 'getAll' };
+
+		$.ajax({
+			type: "POST",
+			url: oUrls.actions,
+			data: data,
+			success: onWishListGetAll,
+			dataType: 'text'
+		});	 
+	});
 
 	 /*****************************************************************************/
 
@@ -42,8 +62,7 @@
 		// Attempt JSON encode
 		try {
 			// Encode the data into JSON
-			var data = JSON.parse(data);
-			console.log(data);
+			var data = JSON.parse(data);			
 		} 
 		// Let's catch any errors
 		catch (e) {
@@ -53,6 +72,79 @@
 		}
 		// END try/catch
 
+		// Get the product title
+		var productTitle = $('.product_title').html();
+
+		// If there are no errors
+		if (data.errorCode > 0) {			
+			// Set the message HTML
+			var messageHtml = '<div class="woocommerce-message mumf-wishlist-message"><a class="button mumf-wishlist-view" href="javascript:void(0)">View wishlist</a> '+ productTitle +' was successfully added to your wishlist.</div>';
+		// Otherwise there are errors
+		} else {
+			// Set the message or use default
+			var message = data.message ? data.message : 'Unable to add product to wishlist';
+			// Set the message HTML
+			var messageHtml = '<div class="woocommerce-error mumf-wishlist-message">'+ message +'</div>';
+		}
+		// END if errors
+
+		// Slide up any errormessages
+		$('.mumf-wishlist-message').slideUp(function() {
+			$(this).remove();
+		});
+
+		// Prepend to #content element
+		$('#content').prepend(messageHtml);
+
+		// Slide down the message
+		$('.mumf-wishlist-message').slideDown();
+
+	}
+
+	// When the response for get all from the server is successful
+	function onWishListGetAll(data) {
+		// Attempt JSON encode
+		try {
+			// Encode the data into JSON
+			var data = JSON.parse(data);			
+		} 
+		// Let's catch any errors
+		catch (e) {
+			console.log('Data is not in JSON format');
+			// Prevent further execution
+			return false;
+		}
+		// END try/catch
+
+		// Check if there's errors
+		if (data.errorCode < 1) {
+			// Set the message or use default
+			var message = data.message ? data.message : 'Unable to retrieve wishlist :(';
+			// Set the message HTML
+			var messageHtml = '<div class="woocommerce-error mumf-wishlist-message">'+ message +'</div>';
+			
+			// Slide up any errormessages
+			$('.mumf-wishlist-message').slideUp(function() {
+				$(this).remove();
+			});
+
+			// Prepend to #content element
+			$('#content').prepend(messageHtml);
+
+			// Slide down the message
+			$('.mumf-wishlist-message').slideDown();
+		}
+		// END if errors
+
+	}
+
+	// Adds a button to the main menu to view the wishlist
+	function addViewWishlistToMenu() {
+		// Set the HTML
+		var html = '<li class="page_item"><a href="javascript:void(0)" class="mumf-wishlist-view">My Wishlist</a></li>';
+
+		// Append to main nav
+		$('#main-nav').append(html);
 	}
 
 	/*****************************************************************************/

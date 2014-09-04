6f47312f75dc97be63bbe0deb841f8cc750bf1ea
Chris Mumford <mumfordnsons@gmail.com>
2014-09-04 12:42:13 +0100

    Updated action.php
    Added cases for getting/adding/deleting wish list items.

--- action.php
+++ action.php
@@ -1,10 +1,15 @@
 <?php
 
+	// Include the wishlist class
+	require_once 'assets/classes/wishlist.class.php';
+
 	// Array to hold response
 	$aResponse = [];
 
 	// Check that we have an action
 	$sAction = isset($_REQUEST['action']) ? $_REQUEST['action'] : null;
+	// Check if we have a wishlist ID
+	$iWishlistId = isset($_REQUEST['id']) ? $_REQUEST['id'] : null;
 
 	// If the action is not present, die
 	if (!$sAction) {
@@ -14,9 +19,88 @@
 	}	
 	// END if
 
+	// If there's a wishlist ID, check it's numeric
+	if ($iWishlistId && !is_numeric($iWishlistId)) {
+		$aResponse['message'] = 'The wishlist ID must be numeric.';
+		$aResponse['errorCode'] = -1;
+		die(json_encode($aResponse));
+	}
+	// END if
+
+
+	// Create instance of wishlist class
+	$myWishlist = new Wishlist();
+
+	// If the action is not getAll
+	if ($sAction != 'getAll') {
+		// Call class function to identify if the product is already in user wishlist
+		$bExists = $myWishlist->getFromWishlist($iWishlistId);
+	}
+
+
+
 	// Switch the action
 	switch ($sAction) {
+
+		// Getting all wishlist items
+		case 'getAll':
+
+			// Otherwise we'll call class function to add it to wishlist
+			$bSuccessful = $myWishlist->getAll();
+
+			// If no rows are returned
+			if (!$bSuccessful) {
+				$aResponse['message'] = 'There are no items in your wishlist.';
+				$aResponse['errorCode'] = -1;
+				die(json_encode($aResponse));
+			}			
+			// END if
+
+			// Otherwise set the response array's data property to the rows returned
+			$aResponse['data'] = $myWishlist->fullWishlist;
+
+			break;
+
+		// Adding a wishlist item
 		case 'add':
-			
+			// If the item exists
+			if ($bExists) {
+				$aResponse['message'] = 'The item is already in your wishlist.';
+				$aResponse['errorCode'] = -1;
+				die(json_encode($aResponse));
+			}			
+			// END if
+
+			// Otherwise we'll call class function to add it to wishlist
+			$bSuccessful = $myWishlist->addToWishlist($iWishlistId);
+
+			// Set the result based on if the add was successful
+			$iResult = $bSuccessful ? 1 : 0;
+
+			// Set the error code to the boolean(1 or 0)
+			$aResponse['errorCode'] = $iResult;			
+			break;
+
+		// Deleting a wishlist item
+		case 'delete':
+			// If the item does not exist
+			if (!$bExists) {
+				$aResponse['message'] = 'The item is not in in your wishlist.';
+				$aResponse['errorCode'] = -1;
+				die(json_encode($aResponse));
+			}	
+			// END if
+
+			// Otherwise we'll call class function to add it to wishlist
+			$bSuccessful = $myWishlist->deleteFromWishlist($iWishlistId);
+
+			// Set the result based on if the add was successful
+			$iResult = $bSuccessful ? 1 : 0;
+
+			// Set the error code to the boolean(1 or 0)
+			$aResponse['errorCode'] = $iResult;							
 			break;
 	}
+
+	// Send the array encoded as JSON
+	echo json_encode($aResponse);
